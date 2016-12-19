3fcc9e4843d1c967230d0aab8a28bbcc2a06adf3
Ciprian Ciubotariu <cheepeero@gmx.net>
2016-12-19 15:20:39 +0200

    Parse GELF UDP message types and handle compression

--- src/GelfUdpServer.cpp
+++ src/GelfUdpServer.cpp
@@ -8,7 +8,11 @@
 
 #include "GelfUdpServer.moc"
 
-GelfUdpServer::GelfUdpServer(const QUrl &url)
+const qint8 GelfUdpServer::compressedZlibMarker[2] = { 0x78, 0x9c };
+const qint8 GelfUdpServer::compressedGzipMarker[2] = { 0x1f, 0x8b };
+const qint8 GelfUdpServer::chunkedMarker[2]        = { 0x1e, 0x0f };
+
+GelfUdpServer::GelfUdpServer(const QUrl& url)
     : listenUrl(url)
 {
     connect(&udpSocket, SIGNAL(readyRead()),
@@ -37,11 +41,76 @@ void GelfUdpServer::readDatagrams()
     }
 }
 
-void GelfUdpServer::processDatagram(const QByteArray &datagram)
+void GelfUdpServer::processDatagram(const QByteArray& datagram)
 {
-    // TODO: check compression
-    // TODO: check chunked message
+    switch (decodeGelfMessageType(datagram)) {
+    case CompressedGzip:
+//         ++stats.receivedCompressedGzip;
+        emit messageReady(uncompressGzip(datagram));
+        break;
+    case CompressedZlib:
+//         ++stats.receivedCompressedZlib;
+        emit messageReady(uncompressZlib(datagram));
+        break;
+    case Uncompressed:
+//         ++stats.receivedUncompressed;
+        // single non-encoded json message
+        emit messageReady(datagram);
+        break;
+    case Chunked:
+//         ++stats.receivedChunked;
+        handleMessageChunk(datagram);
+        break;
+    case Unsupported:
+    default:
+//         ++stats.receivedUnsupported;
+        ;
+    }
+}
 
-    // single non-encoded json message
-    emit messageReady(datagram);
+GelfUdpServer::GelfMessageType GelfUdpServer::decodeGelfMessageType(const QByteArray& datagram)
+{
+    if (datagram.size() < 2) {
+        return Unsupported;
+    }
+
+    qint8 first = datagram.at(0);
+    qint8 second = datagram.at(1);
+
+    if (first == compressedZlibMarker[0]) {
+        // zlib's second byte is for flags and a checksum; it
+        // differs based on compression level used and whether
+        // a dictionary is used.  What the RFC guarantees is
+        // that "CMF and FLG, when viewed as a 16-bit unsigned
+        // integer stored in MSB order (CMF*256 + FLG), is a
+        // multiple of 31"
+        if ((256 * qint16(first) + qint16(second)) % 31 == 0) {
+            return CompressedZlib;
+        }
+    } else if (first == compressedGzipMarker[0] && second == compressedGzipMarker[1]) {
+        return CompressedGzip;
+    } else if (first == chunkedMarker[0] && second == chunkedMarker[1]) {
+        return Chunked;
+    } else {
+        // by default assume the payload to be "raw, uncompressed" GELF, parsing will fail if it's malformed.
+        return Uncompressed;
+    }
+
+    return Unsupported;
+}
+
+QByteArray GelfUdpServer::uncompressGzip(const QByteArray& message)
+{
+    // TODO: this seems to not work out of the box - search internet for workaround
+    return qUncompress(message);
+}
+
+QByteArray GelfUdpServer::uncompressZlib(const QByteArray& message)
+{
+    return qUncompress(message);
+}
+
+void GelfUdpServer::handleMessageChunk(const QByteArray& datagram)
+{
+    // TODO: handle message chunk
 }
--- src/GelfUdpServer.h
+++ src/GelfUdpServer.h
@@ -9,6 +9,13 @@
 #include <QUrl>
 #include <QUdpSocket>
 
+/**
+ * Gelf UDP server.
+ *
+ * Gelf protocol code adapted from:
+ *      https://github.com/Graylog2/graylog2-server/blob/master/graylog2-server/src/main/java/org/graylog2/inputs/codecs/gelf/GELFMessage.java
+ * and companion files there.
+ */
 class GelfUdpServer : public QObject
 {
     Q_OBJECT
@@ -16,20 +23,34 @@ class GelfUdpServer : public QObject
     QUrl listenUrl;
     QUdpSocket udpSocket;
 
-    //
-
-// TODO: chunks
-//    QHash<QByteArray, QHash<uint8_t, QByteArray> > chunks; // incomplete
-
 public:
-    explicit GelfUdpServer ( const QUrl &listenUrl );
+    explicit GelfUdpServer ( const QUrl& listenUrl );
 
 signals:
     void messageReady ( QByteArray gelfMessage );
 
 private slots:
     void readDatagrams();
-    void processDatagram ( const QByteArray &datagram );
+    void processDatagram ( const QByteArray& datagram );
+
+private:
+    enum GelfMessageType {
+        CompressedZlib,
+        CompressedGzip,
+        Chunked,
+        Uncompressed,
+        Unsupported
+    };
+    static const qint8 compressedZlibMarker[2];
+    static const qint8 compressedGzipMarker[2];
+    static const qint8 chunkedMarker[2];
+
+    static GelfMessageType decodeGelfMessageType ( const QByteArray& datagram );
+    static QByteArray uncompressGzip(const QByteArray& message );
+    static QByteArray uncompressZlib(const QByteArray& message );
+
+    void handleMessageChunk ( const QByteArray & datagram );
+
 };
 
 #endif // GELFUDPSERVER_H
